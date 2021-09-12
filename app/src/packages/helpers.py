339d0637cbf1058383199e77ac74df840c013489
Henrique Borsatto de Campos <henry_tuori@hotmail.com>
2021-09-12 17:41:15 -0300

    fix!: Handle missing input_chat appropriately, unfortunately

--- app/src/packages/helpers.py
+++ app/src/packages/helpers.py
@@ -79,7 +79,8 @@ async def format_default_message_text(client : TelegramClient, message : Telegra
 
 async def format_default_unknown_message_text(client : TelegramClient, message_ids : List[int], event : MessageDeleted.Event, tried : bool = False):
     try:
-        chat = await client.get_entity(await event.get_input_chat())
+        input_chat = await event.get_input_chat()
+        chat = await client.get_entity(input_chat) if input_chat else None
     except ValueError:
         if tried:
             raise

f8bbd936f12da01223b72153f4aefdf64383e7bf
henry701 <henry_tuori@hotmail.com>
2024-07-08 01:16:28 -0300

    Fix nginx exiter script

--- docker/server/nginx/custom_run.sh
+++ docker/server/nginx/custom_run.sh
@@ -9,6 +9,6 @@ nginx -c /etc/nginx/nginx.conf -e /dev/stderr &
 
 NGINX_PID="$!"
 
-bash -c "until kill -s 0 "$$" 2>/dev/null; do sleep 1; done && echo "Exiting Nginx!" && kill -9 $NGINX_PID" &
+bash -c "while kill -s 0 "$$" 2>/dev/null; do sleep 1; done && echo 'Exiting Nginx!' && kill -9 "$NGINX_PID &
 
 PORT=8081 exec python3 -m wsgi \"${@}\"
